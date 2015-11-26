d0e38c24e78aef1b9f7b734a99a53213c8ccfaeb
Igor <podobnik.igor@gmail.com>
2015-11-26 21:36:40 +0100

    login urejen, weather urejen, vsak prjavljen samo enkrat na seznam

--- loggedin.py
+++ loggedin.py
@@ -1,5 +1,9 @@
 __author__ = 'Igor'
 from google.appengine.api import users
+from models import Uporabniki
+
+
+
 
 def is_logged_in(P):
     user = users.get_current_user()
@@ -7,14 +11,26 @@ def is_logged_in(P):
     if user:
         logiran = True
         logout_url = users.create_logout_url('/')
-        print user.nickname()
-        print user.user_id()
         print "glej gor"
         paramsif = {"logiran": logiran, "logout_url": logout_url, "user": user}
         Pint.update(paramsif)
+        preverialiobstaja()
     else:
         logiran = False
         login_url = users.create_login_url('/')
         paramsif = {"logiran": logiran, "login_url": login_url, "user": user}
         Pint.update(paramsif)
+
     return Pint
+
+def preverialiobstaja():
+    user = users.get_current_user()
+    emailprejemnika = user.email()
+    user = Uporabniki(user=emailprejemnika)
+    # preverjam ce je user ze v bazi
+    prisoten = Uporabniki.query(Uporabniki.user==emailprejemnika).fetch()
+
+    if prisoten:
+        print "NOTRI JE ZE!"
+    else:
+        user.put()
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import jinja2
 import webapp2
 from loggedin import is_logged_in
 from google.appengine.api import urlfetch
-
-
+from models import Sporocilo,Uporabniki
+import json
+import time
 
 
 
@@ -35,12 +36,50 @@ class BaseHandler(webapp2.RequestHandler):
 class MainHandler(BaseHandler):
     def get(self):
         is_logged_in(params)
+
         return self.render_template("index.html",params=params)
 
+class WeatherHandler(BaseHandler):
+    def get(self):
+        #v urlju so parametri ki jih zelimo videti loceni z ikonco &
+        # novo mesto 3194350 , 3239318 Ljubljana
+        url = "http://api.openweathermap.org/data/2.5/group?id=3196359,3194351,2639110&units=metric&appid=29fb19f38dde3e3bfe9f2c2536b414b0"
+        #zgornji je za vecmest, spodnji je za samo eno mesto
+        #url = "http://api.openweathermap.org/data/2.5/weather?q=Ljubljana&units=metric&appid=29fb19f38dde3e3bfe9f2c2536b414b0"
+        result = urlfetch.fetch(url)
+        podatki = json.loads(result.content)
+        params = {"podatki": podatki}
+        is_logged_in(params)
+        self.render_template("weather.html", params=params)
 
+class RedirectHandler(BaseHandler):
+    def post(self):
+        posiljat = self.request.get("posiljatelj")
+        prejemni = self.request.get("prejemnik")
+        sporocil = self.request.get("sporocilo")
+        if sporocil != "Obvezno vpisi kaj notri":
+            sporoc = Sporocilo(sender=posiljat, reciever=prejemni, message=sporocil)
+            sporoc.put()
+            time.sleep(1)
+        return self.render_template("redirect.html" , params=params)
+
+class CreateHandler(BaseHandler):
+    def get(self):
+        is_logged_in(params)
+        seznamuserjev = Uporabniki.query().fetch()
+        posiljatelj = {"prejemniki": seznamuserjev}
+        params.update(posiljatelj)
+        print "PARAMS"
+        print params
+        return self.render_template("create.html" , params=params)
 
 
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
+    webapp2.Route('/create', CreateHandler),
+    #webapp2.Route('/mymessages', MyMessagesHandler),
+    #webapp2.Route('/sendmessages', SendMessagesHandler),
+    webapp2.Route('/weather', WeatherHandler),
+    webapp2.Route('/redirect', RedirectHandler),
 ], debug=True)
