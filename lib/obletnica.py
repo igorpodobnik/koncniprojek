00f67e91f28966f95ddcc1ee738304f8e8dc7494
Igor <podobnik.igor@gmail.com>
2015-12-02 21:11:00 +0100

    zacetki time stvari

---
+++ lib/obletnica.py
@@ -0,0 +1,32 @@
+__author__ = 'Franko'
+import datetime
+import relativedelta
+
+from datetime import *; from relativedelta import *
+
+
+def izracun(dan,mesec):
+    today = date.today()
+    obletnica = date(today.year,mesec,dan)
+    koliko = obletnica - today
+
+    if koliko.days < 0:
+        novoleto = today.year + 1
+        obletnica=date(novoleto,mesec,dan)
+
+    koliko = obletnica - today
+    return koliko.days
+
+def izracunrojdneva(ime):
+    today = date.today()
+    obletnica = ime
+    obletnica=date(today.year,obletnica.month,obletnica.day)
+    koliko = obletnica - today
+
+
+    if koliko.days < 0:
+        novoleto = today.year + 1
+        obletnica=date(novoleto,obletnica.month,obletnica.day)
+    koliko = obletnica - today
+    return koliko.days
+
---
+++ lib/timecalc.py
@@ -0,0 +1,19 @@
+__author__ = 'Igor'
+import datetime
+import relativedelta
+
+def razlika(ime):
+    now = datetime.now()
+    a = relativedelta(now,ime)
+    return a
+
+"""
+koncno = razlika(igor)
+print koncno.years
+print koncno.months
+print koncno.days
+igor = datetime(1983, 5, 6, 9, 45)
+marusa = datetime(1984, 5, 4, 12, 00)
+now = datetime.now()
+today = date.today()
+"""
